63689f048b69819b35692e692e0bca4008e21c56
Emily Su <emilyzqsu@gmail.com>
2024-02-10 18:04:10 -0500

    Corrected year in replication script

--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -1,6 +1,6 @@
 #### Preamble ####
 # Purpose: Replicated graphs from 
-# "Who Does Voter ID Keep from Voting?" (2021)
+# "Who Does Voter ID Keep from Voting?" (2022)
 # Author: Emily Su
 # Date: 13 February 2024
 # Contact: em.su@mail.utoronto.ca
