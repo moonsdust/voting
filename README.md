3646d80e9294864a1674f8f6af40c33ce9c6197b
Emily Su <emilyzqsu@gmail.com>
2024-02-11 23:36:05 -0500

    Finished replicating figure 2

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ paper, "Who Does Voter ID Keep from Voting?" by Bernard L. Fraga and Michael G. 
 
 The repo is structured as:
 
--   `data/raw_data` contains the raw data from "Who Does Voter ID Keep from Voting?".
+-   `data/raw_data` contains the raw data from the paper, "Who Does Voter ID Keep from Voting?".
 -   `data/analysis_data` contains the cleaned dataset that was constructed.
 -   `other` contains sketches.
 -   `paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
--- scripts/99-replications.R
+++ scripts/99-replications.R
@@ -1,19 +1,81 @@
 #### Preamble ####
-# Purpose: Replicated graphs from 
-# "Who Does Voter ID Keep from Voting?" (2022)
+# Purpose: Replicated graphs from "Who Does Voter ID Keep from Voting?" by 
+# Bernard L. Fraga and Michael G. Miller (2022)
 # Author: Emily Su
 # Date: 13 February 2024
 # Contact: em.su@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: tidyverse and ggplot2 has been installed by running 00-install_packages.R
+# Pre-requisites: tidyverse, ggplot2, and knitr has been installed by running 00-install_packages.R
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(ggplot2)
+library(knitr)
 
 
 #### Load data ####
+rid_data <- read_csv("data/raw_data/rid_data.csv", show_col_types = FALSE)
+county_data <- read_csv("data/raw_data/county_data.csv", show_col_types = FALSE)
+reason_data <- read_csv("data/raw_data/reason_data.csv", show_col_types = FALSE)
+
+#### Replication #### 
+# Table 1
+
+# Figure 2
+figure_2_rep_data <- reason_data |>
+  select(family, applied, transport, birthcert, disability, work, lost, other) |>
+  # Get percentages 
+  mutate( 
+    "Family Obligations" = round((sum(family) / n()) * 100, 2),
+    "Applied, Has Not Recieved" = round((sum(applied) / n()) * 100, 2),
+    "Lack of Transportation" = round((sum(transport) / n()) * 100, 2),
+    "Lacks Necessary Documents" = round((sum(birthcert) / n()) * 100, 2), 
+    "Disability/Illness" = round((sum(disability) / n()) * 100, 2),
+    "Work Obligations" = round((sum(work) / n()) * 100, 2), 
+    "ID was Lost or Stolen" = round((sum(lost) / n()) * 100, 2), 
+    "Other Reason" = round((sum(other) / n()) * 100, 2)
+  ) |>
+  # Remove the original columns
+  select(-c(family, applied, transport, birthcert, disability, work, lost, other)) |>
+  # Have the percentages only show up in 1 row
+  unique() |>
+  # Code snippet to convert to a 1-column matrix and then to a vector 
+  # was adapted from the following: https://www.geeksforgeeks.org/convert-matrix-to-vector-in-r/
+  t() |> # Convert 1-row matrix into a 1-column matrix
+  c() |> # Convert to a vector
+  tibble() |> # Change into a tibble
+  rename( # Left side is the new name and the right side is the old name
+    percent = "c(...)"
+  ) |>
+  mutate(
+    reasons = c("Family Obligations", 
+                "Applied, Has Not Recieved", 
+                "Lack of Transportation", 
+                "Lacks Necessary Documents",
+                "Disability/Illness", 
+                "Work Obligations", 
+                "ID was Lost or Stolen",
+                "Other Reason"
+                )
+  ) 
+
+# Referenced code from https://r-graph-gallery.com/267-reorder-a-variable-in-ggplot2.html
+# To figure out how to sort 
+figure_2 <- figure_2_rep_data |>
+  mutate(reasons = fct_reorder(reasons, desc(percent))) |>
+  ggplot(mapping = aes(x = reasons, y = percent)) +
+  geom_bar(stat="identity") +
+  ggtitle("Figure 2: Impediments cited by Texas voters in 2016") +
+  coord_flip() + 
+  theme_bw() +
+  labs(x = "Reasons", y = "Percent") 
+
+figure_2_table <- 
+  figure_2_rep_data |> 
+  kable()
+
+# Figure 3: 
 
 
 
