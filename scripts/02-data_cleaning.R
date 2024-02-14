afbf33756ca6fd95c2de154a8d80386769c41990
Emily Su <emilyzqsu@gmail.com>
2024-02-14 17:11:04 -0500

    Updated datasets to include female voters and data cleaning script

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -28,7 +28,8 @@ cleaned_reason_data <- reason_data |>
 
 
 # Dataset 2
-# Expected Columns: the_geom | county | count 
+# Expected Columns: 
+# the_geom | county | count | female_count | female_white | female_black | female_latinx | female_asian
 
 # Function to change values in a certain column to uppercase for the first letter of each word
 # Referenced https://www.geeksforgeeks.org/convert-first-letter-of-every-word-to-uppercase-in-r-programming-str_to_title-function/
@@ -39,14 +40,78 @@ toTitle <- function(col) {
   return(col)
 }
 
-# temp dataset to get the count of voters without ID in each county
+# Change county names to be uppercase for only the first word of each part of its name
+cleaned_reason_data$county <- toTitle(cleaned_reason_data$county)
+
+# temp dataset to get the count of voters without ID in each county 
 temp_count_data <- cleaned_reason_data |>
+  # Group by county only to get the count 
   group_by(county) |>
   mutate(
     count = n()
   ) |>
   ungroup() |>
   select(c(county, count)) |>
+  unique()
+
+# Datasets for the count of female voters without ID in each county across each race 
+temp_female_count_data <- cleaned_reason_data |>
+  # Group by county and filter for female to get the female count for voters
+  group_by(county) |>
+  filter(female == 1) |>
+  mutate(
+    female_count = n()
+  ) |>
+  ungroup() |>
+  select(c(county, female_count)) |>
+  unique()
+
+temp_white_female_count_data <- cleaned_reason_data |>
+  # Group by county and filter for female and white
+  group_by(county) |>
+  filter(female == 1) |>
+  filter(white == 1) |>
+  mutate(
+    female_white = n()
+  ) |>
+  ungroup() |>
+  select(c(county, female_white)) |>
+  unique()
+
+temp_black_female_count_data <- cleaned_reason_data |>
+  # Group by county and filter for female and black
+  group_by(county) |>
+  filter(female == 1) |>
+  filter(black == 1) |>
+  mutate(
+    female_black = n()
+  ) |>
+  ungroup() |>
+  select(c(county, female_black)) |>
+  unique()
+
+temp_latinx_female_count_data <- cleaned_reason_data |>
+  # Group by county and filter for female and latino
+  group_by(county) |>
+  filter(female == 1) |>
+  filter(latino == 1) |>
+  mutate(
+    female_latinx = n()
+  ) |>
+  ungroup() |>
+  select(c(county, female_latinx)) |>
+  unique()
+
+temp_asian_female_count_data <- cleaned_reason_data |>
+  # Group by county and filter for female and asian
+  group_by(county) |>
+  filter(female == 1) |>
+  filter(asian == 1) |>
+  mutate(
+    female_asian = n()
+  ) |>
+  ungroup() |>
+  select(c(county, female_asian)) |>
   unique()
 
 cleaned_map_data <- texas_county_boundary_data |>
@@ -59,16 +124,28 @@ cleaned_map_data <- texas_county_boundary_data |>
     county = name
   )
 
-# Change county names to be uppercase for only the first word of each part of its name
-temp_count_data$county <- toTitle(temp_count_data$county)
-cleaned_map_data$county <- toTitle(cleaned_map_data$county)
-
-# join temp_count_data with cleaned_map_data
+# join temporary datasets with cleaned_map_data
 cleaned_map_data <- cleaned_map_data |>
   left_join(temp_count_data, by = "county")
+cleaned_map_data <- cleaned_map_data |>
+  left_join(temp_female_count_data, by = "county")
+cleaned_map_data <- cleaned_map_data |>
+  left_join(temp_white_female_count_data, by = "county")
+cleaned_map_data <- cleaned_map_data |>
+  left_join(temp_black_female_count_data, by = "county")
+cleaned_map_data <- cleaned_map_data |>
+  left_join(temp_latinx_female_count_data, by = "county")
+cleaned_map_data <- cleaned_map_data |>
+  left_join(temp_asian_female_count_data, by = "county")
+
 
 # Set count values that are NA to 0 
 cleaned_map_data$count[is.na(cleaned_map_data$count)] <- 0
+cleaned_map_data$female_count[is.na(cleaned_map_data$female_count)] <- 0
+cleaned_map_data$female_white[is.na(cleaned_map_data$female_white)] <- 0
+cleaned_map_data$female_black[is.na(cleaned_map_data$female_black)] <- 0
+cleaned_map_data$female_latinx[is.na(cleaned_map_data$female_latinx)] <- 0
+cleaned_map_data$female_asian[is.na(cleaned_map_data$female_asian)] <- 0
 
 #### Save data ####
 # Save the cleaned reason data
